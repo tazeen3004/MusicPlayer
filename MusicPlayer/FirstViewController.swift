3bf516948554045c7a5760470ad9e8847b9ebe0a
tazeen khan <tazeenfirojkhan@gmail.com>
2017-02-19 07:41:43 +0530

    Initial Commit

---
+++ MusicPlayer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MusicPlayer
+//
+//  Created by Tazeen on 19/02/17.
+//  Copyright © 2017 Tazeen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MusicPlayer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MusicPlayer
+//
+//  Created by Tazeen on 19/02/17.
+//  Copyright © 2017 Tazeen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
